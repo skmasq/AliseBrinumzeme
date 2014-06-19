f87cb52c4918c3e4e8fc8389d6ae2c469379f1d7
skmasq <silvestrs.kante@outlook.com>
2014-06-19 17:39:12 -0400

    Added starting animations; Positioned elements; structured image folder

--- AliseBrinumzeme/AliseBrinumzeme/Content/js/site/common.js
+++ AliseBrinumzeme/AliseBrinumzeme/Content/js/site/common.js
@@ -1,5 +1,21 @@
 ﻿/// <reference path="~/Scripts/_references.js" />
 
+//#region Utils
+function getFrameLenght(__array, __iterations, __delay) {
+	var resultValue = 0;
+
+	//#region Apply Rules
+	for (i = 0; i < __iterations; i++) {
+		resultValue += __array[i];
+	}
+
+	resultValue += __delay;
+	//#endregion
+
+	return resultValue
+}
+//#endregion
+
 var tween_api = {
 	sections: {
 		thumbnails: {
@@ -35,10 +51,214 @@ var tween_api = {
 				return _self.tl;
 			}
 		}
+	},
+	start: {
+		bubblebird: function (__bird) {
+			/// <summary>
+			/// Total length: 0.55s
+			/// </summary>
+			/// <param name="__bird"></param>
+			/// <returns type=""></returns>
+			var tl = new TimelineMax({
+				paused: true
+			});
+
+			var bird = __bird,
+				legs = __bird.find(".bubble-bird-legs").eq(0);
+
+			/*
+				Speeds:
+					- Bird comes from top
+					- Bird lays down on doors
+					- Bird gets in original position
+			*/
+			var tlSpeeds = [0.40, 0.2, 0.1];
+
+			// Set Objects
+			tl.add(TweenMax.set(bird, {
+				css: {
+					transformOrigin: "100% -200px",
+					rotation: 30,
+					top: -200
+				}
+			}));
+
+			//#region Frame 1
+			tl.add([
+				TweenMax.to(bird, tlSpeeds[0], {
+					css: {
+						top: -53,
+						rotation: 0
+					}
+				}), TweenMax.to(legs, tlSpeeds[0] - 0.2, {
+					css: {
+						height: 20
+					}
+				})
+			]);
+			//#endregion
+
+			//#region Frame 2
+			tl.add(TweenMax.to(legs, 0.2, {
+				css: { height: 8 }
+			}), tlSpeeds[0] - 0.2);
+			//#endregion
+
+			//#region Frame 3
+			tl.add([
+				TweenMax.to(bird, tlSpeeds[2], {
+					css: {
+						top: -56
+					}
+				}), TweenMax.to(legs, tlSpeeds[2], {
+					css: {
+						height: 13
+					}
+				})
+			], tlSpeeds[0]);
+			//#endregion
+
+			return tl;
+		},
+		doorbottomcup: function (__cup) {
+			/// <summary>
+			/// Total length: 0.95s
+			/// </summary>
+			/// <param name="__cup"></param>
+			/// <returns type=""></returns>
+			var tl = new TimelineMax({
+				paused: true
+			});
+
+			var cupbody = __cup.find(".cup-body").eq(0);
+
+			var tlFrameSpeeds = [0.2, 0.2, 0.2, 0.15, 0.1, 0.1]
+
+			// Set objects
+
+			tl.add([
+				TweenMax.set(__cup, {
+					css: {
+						transformOrigin: "57px 52px"
+					}
+				})
+			]);
+
+			//#region Frame 1
+			tl.add(TweenMax.to(cupbody, tlFrameSpeeds[0], {
+				css: { rotation: 7 }
+			}), getFrameLenght(tlFrameSpeeds, 0, 0.1));
+			tl.add(TweenMax.to(__cup, tlFrameSpeeds[0], {
+				css: { rotation: -10 }
+			}), getFrameLenght(tlFrameSpeeds, 0, 0));
+			//#endregion
+
+			//#region Frame 2
+			tl.add(TweenMax.to(cupbody, tlFrameSpeeds[1], {
+				css: { rotation: -7 }
+			}), getFrameLenght(tlFrameSpeeds, 1, 0.1));
+			tl.add(TweenMax.to(__cup, tlFrameSpeeds[1], {
+				css: { rotation: 6 }
+			}), getFrameLenght(tlFrameSpeeds, 1, 0));
+			//#endregion
+
+			//#region Frame 3
+			tl.add(TweenMax.to(cupbody, tlFrameSpeeds[2], {
+				css: { rotation: 4 }
+			}), getFrameLenght(tlFrameSpeeds, 2, 0.1));
+			tl.add(TweenMax.to(__cup, tlFrameSpeeds[2], {
+				css: { rotation: -5 }
+			}), getFrameLenght(tlFrameSpeeds, 2, 0));
+			//#endregion
+
+			//#region Frame 4
+
+			tl.add(TweenMax.to(cupbody, tlFrameSpeeds[3], {
+				css: { rotation: -5 }
+			}), getFrameLenght(tlFrameSpeeds, 3, 0.1));
+			tl.add(TweenMax.to(__cup, tlFrameSpeeds[3], {
+				css: { rotation: 3 }
+			}), getFrameLenght(tlFrameSpeeds, 3, 0));
+
+			//#endregion
+
+			//#region Frame 5
+
+			tl.add(TweenMax.to(cupbody, tlFrameSpeeds[4], {
+				css: { rotation: -3 }
+			}), getFrameLenght(tlFrameSpeeds, 4, 0.1));
+			tl.add(TweenMax.to(__cup, tlFrameSpeeds[4], {
+				css: { rotation: 0 }
+			}), getFrameLenght(tlFrameSpeeds, 4, 0));
+
+			//#endregion
+
+			//#region Frame 6
+
+			tl.add(TweenMax.to(cupbody, tlFrameSpeeds[5], {
+				css: { rotation: 0 }
+			}), getFrameLenght(tlFrameSpeeds, 5, 0));
+
+			//#endregion
+
+			return tl;
+		},
+		doorrabbit: function (__rabbit) {
+			/// <summary>
+			/// Total length: 0.5s
+			/// </summary>
+			/// <param name="__rabbit"></param>
+			/// <returns type=""></returns>
+			var tl = new TimelineMax({
+				paused: true
+			});
+
+			var tlFrameLength = [0.5]
+
+			// Set object
+			tl.add(TweenMax.set(__rabbit, {
+				css: { bottom: 100, left: 70 }
+			}));
+
+			//#region Frame 1
+			tl.add(TweenMax.to(__rabbit, tlFrameLength[0], {
+				css: { bottom: 147, left: -58 },
+				ease: Back.easeOut
+			}));
+			//#endregion
+
+			return tl;
+		},
+		molbertrabbit: function (__rabbit) {
+			/// <summary>
+			/// Total length: 0.35s
+			/// </summary>
+			/// <param name="__rabbit"></param>
+			/// <returns type=""></returns>
+			var tl = new TimelineMax({
+				paused: true
+			});
+
+			var tlFrameLength = [0.35]
+
+			// Set object
+			tl.add(TweenMax.set(__rabbit, {
+				css: { bottom: -40 }
+			}));
+
+			//#region Frame 1
+			tl.add(TweenMax.to(__rabbit, tlFrameLength[0], {
+				css: { bottom: 45 },
+				ease: Back.easeOut
+			}));
+			//#endregion
+
+			return tl;
+		}
 	}
 }
 
-var test = function () {
+var bubbleBirdBeakAnimation = function () {
 
 	/* Bird's beak animation*/
 	var tl = new TimelineLite({ paused: true });
@@ -48,35 +268,38 @@ var test = function () {
 	var topBeak = $(".bubble-bird-top-beak"),
 		bottomBeak = $(".bubble-bird-bottom-beak");
 
-	tl
-		.from(topBeak, 0.2, { rotation: "0", }, 0, "openTop")
-		.from(bottomBeak, 0.2, { rotation: "0", }, 0.3, "openTop")
-		.to(topBeak, 0.2, {
-			rotation: "-=15",
-			transformOrigin: "left 0",
+	tl.add([
+		TweenMax.set([topBeak, bottomBeak], {
+			css: {
+				transformOrigin: "left 0",
+				rotation: 0
+			}
+		})
+	]);
+
+	tl.add([
+		TweenMax.to(topBeak, 0.2, {
+			css: {
+				rotation: -15
+			},
 			repeat: sk,
 			ease: Power2.easeInOut
-		}, 0.3, "openTop"
-      )
-		.to(bottomBeak, 0.2, {
-			rotation: "+=15",
-			transformOrigin: "left 0",
+		}),
+		TweenMax.to(bottomBeak, 0.2, {
+			css: {
+				rotation: 15
+			},
 			repeat: sk,
 			ease: Power2.easeInOut
-		}, 0.3, "openBottom");
+		})
+	]);
 
-	tl.timeScale(1);
 
-	return {
-		play: function () {
-			tl.restart().play().eventCallback("onComplete", function () {
-				tl.restart().pause();
-			});
-		}
-	}
+	return tl;
 }
 
-var test1 = new test();
+
+var test1 = new bubbleBirdBeakAnimation();
 
 $(document).ready(function () {
 	$('.main-scene-container').find("img").eq(0).show();
@@ -85,8 +308,8 @@ $(document).ready(function () {
 	initBGSwitch();
 	initThumbnails();
 	bubbleTextInit();
-	birdInit();
-	lampInit();
+	//birdInit();
+	//lampInit();
 });
 
 function lampInit() {
@@ -273,13 +496,16 @@ function bubbleTextInit() {
 	$(document).on("mouseenter", ".menu-hitbox", function () {
 		var $ele = $(this);
 		comonTimer = setTimeout(function () {
-			test1.play();
-			$("[data-bubbletextindex=" + $ele.data("menutextindex") + "]").fadeIn();
+			test1.play().eventCallback("onStart", function (something) {
+				$("[data-bubbletextindex=" + $ele.data("menutextindex") + "]").fadeIn(200);
+			}).eventCallback("onComplete", function () {
+				test1.restart().pause();
+			});
 		}, 300);
 	}).on("mouseleave", ".menu-hitbox", function () {
 		var $ele = $(this);
 		clearTimeout(comonTimer);
-		$("[data-bubbletextindex=" + $ele.data("menutextindex") + "]").stop().fadeOut();
+		$("[data-bubbletextindex=" + $ele.data("menutextindex") + "]").stop().fadeOut(100);
 	});
 }
 
